572b319cdcca5082ca43801c8dc83d50ad287a9e
Emily O'Marra <emilyomarra@hotmail.com>
2023-02-14 00:09:39 +0900

    rails yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
+  root to: 'restaurants#index'
   # root "articles#index"
+  # get '/restaurants', to: 'restaurants#index', as: :restaurants
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post '/restaurants', to: 'restaurants#create'
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get '/restaurants/:id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  # post '/restaurants/:id/reviews', to: 'reviews#create'
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
